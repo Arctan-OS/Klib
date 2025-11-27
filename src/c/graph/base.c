c475d5abe7cc26fba417826c610ee2f2d34df5cb
awewsomegamer <awewsomegamer@gmail.com>
2025-11-27 16:15:46 -0600

    base: fix graph_find, correct graph_remove
    The graph_find function has been fixed and now works, just needed to add
    in/reorder a few if statements. The graph_remove function also now
    increments the reference counter of the node that it is removing. This
    prevents other remove operations from meddling with the current one.

--- src/c/graph/base.c
+++ src/c/graph/base.c
@@ -24,6 +24,7 @@
  *
  * @DESCRIPTION
 */
+#include "global.h"
 #include "lib/atomics.h"
 #include "lib/graph/base.h"
 #include "lib/util.h"
@@ -69,9 +70,7 @@ int graph_add(ARC_GraphNode *parent, ARC_GraphNode *node, char *_name) {
 }
 
 static int graph_recursive_free(ARC_GraphNode *node) {
-        int rc = ARC_ATOMIC_LOAD(node->ref_count);
-
-        if (rc > 0) {
+        if (ARC_ATOMIC_LOAD(node->ref_count) > 1) {
                 return -1;
         }
 
@@ -98,23 +97,24 @@ int graph_remove(ARC_GraphNode *node, bool free) {
                 return -1;
         }
 
-        int rc = ARC_ATOMIC_LOAD(node->ref_count);
-
-        if (rc > 0) {
+        if (ARC_ATOMIC_INC(node->ref_count) > 1) { // NOTE: Prevents other remove operations
                 return -2;
         }
 
         ARC_GraphNode *parent = node->parent;
 
-        if (parent != NULL) {
+        if (parent != NULL && ARC_ATOMIC_LOAD(parent->child) == node) {
                 ARC_ATOMIC_XCHG(&parent->child, &node->next, &node->next);
-                ARC_ATOMIC_DEC(parent->child_count);
                 node->parent = NULL;
                 // node->next = node; If not, something went wrong
         }
 
-        if (free && graph_recursive_free(node) != 0) {
-                return -3;
+        ARC_ATOMIC_DEC(parent->child_count);
+
+        if (free) {
+                return graph_recursive_free(node) - 2;
+        } else {
+                ARC_ATOMIC_DEC(node->ref_count);
         }
 
         return 0;
@@ -125,63 +125,101 @@ ARC_GraphNode *graph_find(ARC_GraphNode *parent, char *targ) {
                 return NULL;
         }
 
+        ARC_DEBUG(INFO, "-- Finding %s under %p --\n", targ, parent);
+
         full_recheck:;
         ARC_ATOMIC_INC(parent->ref_count);
         size_t a = ARC_ATOMIC_LOAD(parent->child_count);
 
+        ARC_DEBUG(INFO, "a=%lu\n", a);
+
         ARC_GraphNode *current = ARC_ATOMIC_LOAD(parent->child);
+
+        if (current == NULL) {
+                return NULL;
+        }
+        
+        ARC_GraphNode *initial = current;
+        
+        ARC_DEBUG(INFO, "current=%p\n", current);
         ARC_ATOMIC_INC(current->ref_count);
+
         while (current != NULL && strcmp(targ, current->name) != 0) {
+                ARC_DEBUG(INFO, "%s, %p : %s\n", current->name, current, targ);
                 ARC_GraphNode *t = current;
                 current = ARC_ATOMIC_LOAD(current->next);
-                ARC_ATOMIC_INC(current->ref_count);
-                ARC_ATOMIC_DEC(t->ref_count);
-
+                ARC_DEBUG(INFO, "current=%p\n", current);
                 if (current == t) {
-                        break;
+                        ARC_DEBUG(INFO, "Current == Last, full recheck needed\n");
+                        goto full_recheck;
                 }
+
+                if (current != NULL) {
+                        ARC_ATOMIC_INC(current->ref_count);
+                }
+                ARC_ATOMIC_DEC(t->ref_count);
         }
 
         size_t b = ARC_ATOMIC_LOAD(parent->child_count);
 
+        ARC_DEBUG(INFO, "b=%lu\n", b);
+
         if (current != NULL) {
+                ARC_DEBUG(INFO, "Found %s under %p as %p\n", targ, parent, current);
                 ARC_ATOMIC_DEC(parent->ref_count);
+
                 return current;
         }
 
-        ARC_ATOMIC_DEC(current->ref_count);
-
         size_t delta = b - a;
+        ARC_DEBUG(INFO, "delta=%lu\n", delta);
 
         if (a > b) {
+                ARC_DEBUG(INFO, "Nodes have been removed, rechecking all\n");
                 // Nodes have been removed, potentially added, recheck every node
                 goto full_recheck;
         } else if (a == b) {
+                ARC_DEBUG(INFO, "No change, rechecking first element\n");
                 // Check the current child node, most likely one node was removed and
                 // potentially the target node added
                 delta = 1;
         } else {
+                ARC_DEBUG(INFO, "Nodes added, rechecking on delta\n");
                 // Nodes have been added, recheck delta
         }
 
         current = ARC_ATOMIC_LOAD(parent->child);
+
+        if ((delta == 1 && current == initial) || current == NULL) {
+                ARC_DEBUG(INFO, "Absolutely no change, no need to recheck\n");
+                return NULL;
+        }
+
+        ARC_DEBUG(INFO, "current=%p\n", current);
         ARC_ATOMIC_INC(current->ref_count);
+
         int r = -1;
         while (current != NULL && (r = strcmp(targ, current->name)) != 0 && delta > 0) {
                 ARC_GraphNode *t = current;
                 current = ARC_ATOMIC_LOAD(current->next);
-                ARC_ATOMIC_INC(current->ref_count);
-                ARC_ATOMIC_DEC(t->ref_count);
-                delta--;
 
                 if (current == t) {
-                        break;
+                        ARC_DEBUG(INFO, "Current == Last, full recheck needed\n");
+                        goto full_recheck;
                 }
+
+                if (current != NULL) {
+                        ARC_ATOMIC_INC(current->ref_count);
+                }
+                ARC_ATOMIC_DEC(t->ref_count);
+
+                delta--;
         }
 
         ARC_ATOMIC_DEC(parent->ref_count);
 
         if (r == 0) {
+                ARC_DEBUG(INFO, "Found node %p\n", current);
                 return current;
         }
 
