4613055bd2ef89f9de864484be0ca3b50e862a77
awewsomegamer <awewsomegamer@gmail.com>
2026-02-16 19:01:31 -0600

    cache: framework
    I am beginning to work out a framework for a generic caching mechansim.

---
+++ src/c/cache/base.c
@@ -0,0 +1,5 @@
+// Generic structures and functions for creating and managing cachce
+// cache *init(management_function, eviction_hook);
+// manual_eviction(cache *, what);
+// cache_add(cache *, what, value);
+// cache_delete(cache *);
---
+++ src/c/cache/mru.c
@@ -0,0 +1,1 @@
+// Most recently used
